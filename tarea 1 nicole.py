9581d301d06b97fade33cf2aeb1ea2bbd1c9caa9
nicoleGutiereez <nicoleespinag1@gmail.com>
2025-10-06 11:47:22 -0400

    Add files via upload

---
+++ tarea 1 nicole.py
@@ -0,0 +1,17 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("hola, soy nicole")
+nombre "nicole gutierrez"
+edad 19
+print(f"el nombre es" {nombre} y tengo {edad} años")
+
+numero1 = int(input("cual es el primer numero?"))
+numero2 = int(input("cual es el segundo numero?"))
+suma = numero1 + numero2
+resta = numero2 - numero1
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
